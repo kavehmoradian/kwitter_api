a8b0c848035212eb378a9c99f07b2634f1609b0f
kaveh moradian <kave.moradian@gmail.com>
2023-07-16 00:28:30 +0330

    add bcrypt

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -2,15 +2,22 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from 'src/modules/auth/auth.service';
 import { LoginDto } from 'src/requests/auth/login.dto';
 import { RegisterDto } from 'src/requests/auth/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('register')
-  register(@Body() data: RegisterDto) {
+  async register(@Body() data: RegisterDto) {
+    const salt = await bcrypt.genSalt();
+    data.password1 = await bcrypt.hash(data.password1, salt);
+
     return data;
   }
 
   @Post('login')
-  login(@Body() data: LoginDto) {}
+  async login(@Body() data: LoginDto) {
+    const match = await bcrypt.compare(data.password, '');
+    return match;
+  }
 }
